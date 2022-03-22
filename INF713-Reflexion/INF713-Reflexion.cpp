0ba6432ad52faa5742d9975b106c53cc5160ba75
Mathieu Charron <mathieucharron42@gmail.com>
2022-03-21 21:27:38 -0400

    Implement reflection repl example

--- INF713-Reflexion/INF713-Reflexion.cpp
+++ INF713-Reflexion/INF713-Reflexion.cpp
@@ -1,20 +1,53 @@
-// INF713-Reflexion.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "Potato.h"
+
+#include "ReflectionRepl.h"
+
+#include "Type/TypeDescriptor.h"
+#include "Type/TypeDescriptorFactory.h"
+#include "Type/TypeLibrary.h"
+#include "Type/TypeLibraryFactory.h"
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <stdint.h>
+#include <iosfwd>
+
+using namespace Reflecto;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const Reflection::TypeLibrary typeLibrary = Reflection::TypeLibraryFactory()
+		.Add<std::string>("string")
+		.Add<float>("float")
+		.Add<int32_t>("int32")
+		.Add<bool>("bool")
+		.Add<Potato>("Potato")
+	.Build();
+
+	Reflection::TypeDescriptor potatoTypeDescriptor = Reflection::TypeDescriptorFactory<Potato>(typeLibrary)
+		.RegisterMember(&Potato::Type, "Type")
+		.RegisterMember(&Potato::Weight, "Weight")
+		.RegisterMember(&Potato::Calories, "Calories")
+		.RegisterMember(&Potato::Sodium, "Sodium")
+		.RegisterMember(&Potato::IsBaked, "IsBaked")
+		.RegisterMethod(&Potato::Bake, "Bake")
+		.RegisterMethod(&Potato::AddButter, "AddButter")
+		.RegisterMethod(&Potato::AddSalt, "AddSalt")
+	.Build();
+
+	Serialization::Serializer serializer = Serialization::SerializerFactory(typeLibrary)
+		.LearnType<int32_t, Serialization::Int32SerializationStrategy>()
+		.LearnType<std::string, Serialization::StringSerializationStrategy>()
+		.LearnType<float, Serialization::FloatSerializationStrategy>()
+		.LearnType<bool, Serialization::BooleanSerializationStrategy>()
+	.Build();
+
+	Potato instance = Potato::BuildRusset();
+
+	auto& input = std::cin;
+	auto& ouput = std::cout;
+
+	Reflection::Repl<Potato> repl;
+	repl.Run(instance, potatoTypeDescriptor, serializer, input, ouput);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
